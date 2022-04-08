6b85c652eea85b0e92f135edef1abb2b85de9ab5
howtoapple <74847942+howtoapple@users.noreply.github.com>
2022-04-07 22:42:05 -0600

    Stage

---
+++ helloWorld.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld
+{
+    class helloWorld {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
